54fe046b0c1f531d549ccd26b63e31d6897a1a9d
dilham22 <aidililhamwerfete@gmail.com>
2025-03-26 03:14:04 +0700

    Membuat koneksi ke database

--- zakat.py
+++ zakat.py
@@ -1,23 +1,11 @@
+import mysql.connector
 import pandas as pd
-import os
 
-# Data awal
-harga_beras = 15000  # Harga default per kg
-data_zakat = []
-filename = "data_zakat.xlsx"
 
-def tampilkan_harga_beras():
-    print(f"\nHarga beras saat ini: Rp {harga_beras:,} per kg\n")
-
-def input_harga_beras():
-    global harga_beras
-    try:
-        baru = int(input("Masukkan harga beras baru per kg: "))
-        if baru > 0:
-            harga_beras = baru
-            print("Harga beras berhasil diperbarui!")
-            tampilkan_harga_beras()
-        else:
-            print("Harga harus lebih dari 0!")
-    except ValueError:
-        print("Input harus berupa angka!")
+def create_connection():
+    return mysql.connector.connect(
+        host="localhost",      
+        user="root",           
+        password="",   
+        database="zakat" 
+    )
